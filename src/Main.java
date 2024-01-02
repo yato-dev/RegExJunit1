60766a875013dac9d4d6f132af83420a5d36c3e7
Shikhar Barthwal <barthwalshikhar@gmail.com>
2024-01-03 02:21:11 +0530

    uc1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Shikhar");
+        System.out.println(user);
+        User user1 = new User("Barthwal");
+        System.out.println(user1);
+    }
+}
