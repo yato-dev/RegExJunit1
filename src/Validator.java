6b1744d286223dbd6d167a13d33e0bcebee27ab9
Shikhar Barthwal <barthwalshikhar@gmail.com>
2024-01-03 02:39:13 +0530

    uc rule 2

--- src/Validator.java
+++ src/Validator.java
@@ -1,4 +1,5 @@
 
+
 import java.util.regex.Pattern;
 
 public class Validator {
@@ -32,7 +33,7 @@ public class Validator {
     }
 
     public boolean validatePassword(String password) {
-        Pattern pattern = Pattern.compile(".{8,}");
+        Pattern pattern = Pattern.compile("^(?=.*[A-Z]).{8,}$");
         boolean matcher = pattern.matcher(password).matches();
         if (!matcher)
             System.out.println("Email format is incorrect");
