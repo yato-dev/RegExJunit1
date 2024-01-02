223c8b24cedfa2039779f32dfb9a6f9b5c137365
Shikhar Barthwal <barthwalshikhar@gmail.com>
2024-01-03 02:37:32 +0530

    uc rule1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
         public static void main(String[] args) {
-            User user = new User("Shikhar", "Barthwal", "abc.tyu@cv.co.in", "91 123456789");
+            User user = new User("Shikhar", "Barthwal", "abc.tyu@cv.co.in", "91 123456789", "avcgn645767");
          
         }
     
--- src/User.java
+++ src/User.java
@@ -1,14 +1,13 @@
 
-import java.util.regex.Pattern;
-
 public class User {
     String firstName;
     String lastName;
     String email;
     String phone;
+    String password;
 
-    User(String firstName, String lastName, String email, String phone) {
-        if (!validate(firstName, lastName, email, phone)) {
+    User(String firstName, String lastName, String email, String phone, String password) {
+        if (!validate(firstName, lastName, email, phone, password)) {
 
             throw new IllegalArgumentException();
         }
@@ -16,18 +15,21 @@ public class User {
         this.lastName = lastName;
         this.email = email;
         this.phone = phone;
+        this.password = password;
         System.out.println("User created successfully : " + this.toString());
     }
 
-    private boolean validate(String firstName, String lastName, String email, String phone) {
+    private boolean validate(String firstName, String lastName, String email, String phone, String password) {
         Validator validator = new Validator();
         return validator.validateName(firstName) && validator.validateName(lastName) && validator.validateEmail(email)
-                && validator.validatePhone(phone);
+                && validator.validatePhone(phone) && validator.validatePassword(password);
     }
 
     @Override
     public String toString() {
-        return "firstName: " + firstName + ", lastName: " + lastName + ", email: " + email + ", Phone:" + phone;
+        return "firstName: " + firstName + ", lastName: " + lastName
+                + ", email: " + email + ", Phone: " + phone
+                + ", Password: " + password;
     }
 
 }
