7bd9781b173678de5f6d3d280509c4145d1f14cb
Shikhar Barthwal <barthwalshikhar@gmail.com>
2024-01-04 01:32:01 +0530

    junit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,4 +1,5 @@
 package com.example;
+
 public class Main {
     public static void main(String[] args) {
         User user = new User("Shikhar", "Barthwal", "abc.tyu@cv.co.in", "91 123456789", "avcgn645767");
