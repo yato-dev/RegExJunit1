cc1c2e907939a08bcb783c81ca961616c1907232
Shikhar Barthwal <76698852+yato-dev@users.noreply.github.com>
2024-01-04 02:29:31 +0530

    Revert "Merge pull request #5 from yato-dev/exceptions-1"
    This reverts commit b7d2fccd86acfa13a3d2b66d935667a29abd9bcc, reversing
    changes made to bd4962120e5c4d785edabb1825b78d3af0835b4a.

--- InvalidDataException.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class InvalidDataException extends Exception {
-    public InvalidDataException(String message) {
-        super(message);
-    }
-}
--- InvalidUserDetailsException.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class InvalidUserDetailsException extends Exception {
-
-    public InvalidUserDetailsException(String message) {
-        super(message);
-    }
-}
